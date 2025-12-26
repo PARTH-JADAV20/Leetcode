1686cc4d16507c1bd76649fb55f76ebf6c1613b4
PARTH JADAV <parthjadav.cg@gmail.com>
2025-12-26 10:26:57 +0530

    12-26-2025 0875-koko-eating-bananas Medium

---
+++ LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas-12-26-2025-10-26-58.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxPile = *max_element(piles.begin(), piles.end());
+
+        int low = 1, high = maxPile;
+        int ans = maxPile;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            long long totalH = calculateTotalHours(piles, mid);
+
+            if (totalH <= h) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    int calculateTotalHours(vector<int>& piles, int speed) {
+        long long totalH = 0;
+        for (int bananas : piles) {
+            totalH += (bananas + speed - 1) / speed;
+        }
+        return totalH;
+    }
+};
