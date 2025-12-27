882e367978ccb09c10deb990077d74991c33d9b8
PARTH JADAV <parthjadav.cg@gmail.com>
2025-12-28 00:19:07 +0530

    12-28-2025 0410-split-array-largest-sum Hard

---
+++ LeetCode/Hard/0410-split-array-largest-sum/0410-split-array-largest-sum-12-28-2025-00-19-09.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(),0);
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int partitions = countPartitions(nums, mid);
+
+            if (partitions > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+    int countPartitions(const vector<int> a, int maxSum) {
+        int partitions = 1; 
+        long subarraySum = 0; 
+
+        for (int num : a) {
+            if (subarraySum + num <= maxSum) {
+                subarraySum += num;
+            } else {
+                partitions++;
+                subarraySum = num;
+            }
+        }
+        return partitions;
+    }
+};
