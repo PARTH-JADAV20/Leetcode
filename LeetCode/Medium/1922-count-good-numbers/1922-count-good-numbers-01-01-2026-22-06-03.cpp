3e739044b8150d26abf917d46808972b18c4d991
PARTH JADAV <parthjadav.cg@gmail.com>
2026-01-01 22:06:00 +0530

    01-01-2026 1922-count-good-numbers Medium

---
+++ LeetCode/Medium/1922-count-good-numbers/1922-count-good-numbers-01-01-2026-22-06-03.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static const int MOD = 1e9 + 7;
+
+    long long fastPow(long long base, long long exp) {
+        long long result = 1;
+
+        while (exp > 0) {
+            if (exp % 2 == 1)
+                result = (result * base) % MOD;
+
+            base = (base * base) % MOD;
+            exp /= 2;
+        }
+        return result;
+    }
+
+    int countGoodNumbers(long long n) {
+        long long evenCount = (n + 1) / 2;
+        long long oddCount = n / 2;
+
+        long long ans =
+            (fastPow(5, evenCount) * fastPow(4, oddCount)) % MOD;
+
+        return ans;
+    }
+};
