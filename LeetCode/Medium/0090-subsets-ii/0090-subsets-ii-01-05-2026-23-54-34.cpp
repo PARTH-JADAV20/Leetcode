a1ff3f282eefcdf27f3e2b0f968043c7e84c9b87
PARTH JADAV <parthjadav.cg@gmail.com>
2026-01-05 23:54:35 +0530

    01-05-2026 0090-subsets-ii Medium

---
+++ LeetCode/Medium/0090-subsets-ii/0090-subsets-ii-01-05-2026-23-54-34.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+   void findSubsets(int start, vector<int>& nums, vector<int>& current, vector<vector<int>>& result) {
+        result.push_back(current);
+
+        for (int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            current.push_back(nums[i]);
+            findSubsets(i + 1, nums, current, result);
+            current.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> result;
+        vector<int> current;
+
+        findSubsets(0, nums, current, result);
+
+        return result;
+    }
+};
