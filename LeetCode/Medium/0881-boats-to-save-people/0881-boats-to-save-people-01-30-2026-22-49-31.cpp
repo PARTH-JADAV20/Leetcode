c2d5f2d694281bfe5e32b3f85dd2867a410295f7
PARTH JADAV <parthjadav.cg@gmail.com>
2026-01-30 22:49:26 +0530

    01-30-2026 0881-boats-to-save-people Medium

---
+++ LeetCode/Medium/0881-boats-to-save-people/0881-boats-to-save-people-01-30-2026-22-49-31.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n=people.size();
+        int boats=0;
+        sort(people.begin(),people.end());
+       int l=0 , r=n-1;
+       while(l<=r)
+       {
+         if(people[l]+people[r]<=limit)
+         {
+            boats++;
+            l++;
+            r--;
+         }
+        else
+         {
+            boats+=1;
+            r--;
+         }
+       }
+       return boats;
+    }
+};
