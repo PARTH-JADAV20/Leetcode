cca0b3d64ab6b99c53b95c40730ef49113e1c519
PARTH JADAV <parthjadav.cg@gmail.com>
2026-02-19 22:37:04 +0530

    02-19-2026 0461-hamming-distance Easy

---
+++ LeetCode/Easy/0461-hamming-distance/0461-hamming-distance-02-19-2026-22-37-08.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int num = x ^ y;  
+        int count = 0;
+
+        while (num > 0) {
+            count += (num & 1);  
+            num = num >> 1;     
+        }
+
+        return count;
+    }
+};
